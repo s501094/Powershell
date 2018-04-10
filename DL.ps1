71494f13768e7b9bdf64072b74ed8f9b29e1cc62
Ty Ellis <tyellis1111245@gmail.com>
2018-04-10 10:16:46 -0500

    DL ManagedBy change
    change the manager of a group of DL's

---
+++ DL.ps1
@@ -0,0 +1,4 @@
+﻿$DLTeam = Get-DistributionGroup -Filter {ManagedBy -eq "Intra.infousa.com/Corporate/Special/Users/Abdoo, Earl"}
+foreach ($team in $DLTeam) {
+    Set-DistributionGroup $team -ManagedBy "intra.infousa.com/Corporate/infoUSA/Papillion/National Accounts/Users/Jezerski, Tracey"
+}
