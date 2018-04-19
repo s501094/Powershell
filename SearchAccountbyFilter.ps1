b8fa85130a016cf5faa8814646cf47a6b4b3d49d
Ty Ellis <tyellis1111245@gmail.com>
2018-04-19 10:11:03 -0500

    work in progress
    work in progress.

---
+++ SearchAccountbyFilter.ps1
@@ -0,0 +1,18 @@
+﻿<#	
+=================================================================================
+# Created with: 	Powershell v5.1												#
+# Created on:   	3/28/2018													#
+# Created by:   	tylere														#
+# Organization: Infogroup/Landesk Engineering									#
+# Filename: Contractor list populate											#
+# Discription: searches for users based off contractor listing in office site	#
+=================================================================================
+#>
+
+Import-Module ActiveDirectory
+
+Get-ADUser -filter { physicalDeliveryOfficeName -eq 'Offsite/Contractor' } |
+
+Select-Object SamAccountName |
+
+Export-CSV -Path '\\intra.infousa.com\stc\Common\tylereScripttest\Contractors\contractorlist.csv'
