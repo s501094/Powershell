7038eeb5d7342f3b8ec0b5b9c33ea3a1b6490842
Ty Ellis <tyellis1111245@gmail.com>
2018-04-19 10:06:24 -0500

    changing name
    changing name

--- DL.ps1
+++
@@ -1,4 +0,0 @@
-﻿$DLTeam = Get-DistributionGroup -Filter {ManagedBy -eq "Intra.infousa.com/Corporate/Special/Users/Abdoo, Earl"}
-foreach ($team in $DLTeam) {
-    Set-DistributionGroup $team -ManagedBy "intra.infousa.com/Corporate/infoUSA/Papillion/National Accounts/Users/Jezerski, Tracey"
-}
